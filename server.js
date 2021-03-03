a120a292ceaaf51a2430600e79b3a722c37e245d
GovinduChathumina <46335009+GovinduChathumina@users.noreply.github.com>
2021-03-04 00:54:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,26 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require('mongoose')
 
-var corsOptions = {
-    origin: "http://vedioclay-001-o6obi.ondigitalocean.app"
-  };
+// var corsOptions = {
+//     origin: "http://vedioclay-001-o6obi.ondigitalocean.app"
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE");
+    return res.status(200).json({
+      message: "it works"
+    });
+  }
+  next();
+});
   
   // parse requests of content-type - application/json
   app.use(bodyParser.json());
